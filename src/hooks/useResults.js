7ed453a37ad0cf08aad99ed756983ee13aefd3b9
Andrew Ammon <andrewammon@Andrews-MacBook-Pro-2.local>
2020-05-09 19:53:01 -0400

    react native demo

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import yelp from "../api/yelp";
+
+export default useResults = () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async searchTerm => {
+    console.log("hi there");
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "long beach, ny"
+        }
+      });
+      setResults(response.data.businesses);
+      setErrorMessage("");
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
